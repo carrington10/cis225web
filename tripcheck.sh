86505db486ec998e80dcfdb1ebde4681a2661803
carrington Cooper <carrington10@ksu.edu>
2015-12-14 11:22:05 -0600

    First commit

---
+++ tripcheck.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo "please enter a file name" 
+read  filename 
+if [ $filename = "tripwire" ];
+then
+echo "error file exist "
+exit
+else
+tripwire -m c > $HOME/$filename
+diff -y $HOME/$filename $HOME/tripwire.txt > $HOME/$filename.diff
+exit 
+fi
